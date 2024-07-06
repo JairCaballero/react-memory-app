803492a87b1089f899aeeb6e1e89add8baa2f026
JairCaballero <dwancaballero@gmail.com>
2024-07-05 22:24:32 -0500

    chore: cambios layout

--- src/layouts/LayoutPublic.jsx
+++ src/layouts/LayoutPublic.jsx
@@ -4,7 +4,7 @@ const LayoutPublic = () => {
   return (
     <div className='w-screen h-screen'>
       <div className='flex items-center justify-center h-auto w-full'>
-        <div className='container 2xl:max-w-[1280px] mx-auto p-[2rem]'>
+        <div className='container 2xl:max-w-[1280px] mx-auto'>
           <Outlet />
         </div>
       </div>
